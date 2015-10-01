d25983155a0dd23a7dd4464b2f8693bf206567e3
Januar <januar.srt@gmail.com>
2015-10-01 22:38:08 +0700

    api url

--- src/org/medankulinar/event/AppConfig.java
+++ src/org/medankulinar/event/AppConfig.java
@@ -9,7 +9,7 @@ public class AppConfig {
  /* ganti alamat ini sesuai dengan server webservicenya 
   * jika mau dicoba di emulator, ip tetap 10.0.2.2
   */
-	public static final String SERVER = "http://medankulinar-freepro.rhcloud.com/api/";
+	public static final String SERVER = "http://medanar-freepro.rhcloud.com/";
 //	public static final String SERVER = "http://192.168.137.1/medankulinar/api/";
 	public static final String GET_LIST = "index/getlistpoi";
 	public static final String GET_DETAIL = "index/getDetailPoi";
